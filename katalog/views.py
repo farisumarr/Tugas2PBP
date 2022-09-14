ed3c761f1f98da4a0839e0b771d36ec676adb2cf
farisumarr <muhammadfarisumarrahman@gmail.com>
2022-09-14 22:03:35 +0700

    Push 1 Tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'nama': 'Muhamamd Faris Umar Rahman',
+    'npm' : '2106702402',
+    'list_barang': data_barang_katalog
+}
+    return render(request, "katalog.html", context)

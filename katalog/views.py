87765e0366840a8b44677c7207a0d300a4228b8e
farisumarr <muhammadfarisumarrahman@gmail.com>
2022-09-14 22:45:21 +0700

    benerin typo nama

--- katalog/views.py
+++ katalog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
 
+
 def show_katalog(request):
     data_barang_katalog = CatalogItem.objects.all()
     context = {
-    'nama': 'Muhamamd Faris Umar Rahman',
-    'npm' : '2106702402',
-    'list_barang': data_barang_katalog
-}
+        'nama': 'Muhammad Faris Umar Rahman',
+        'npm': '2106702402',
+        'list_barang': data_barang_katalog
+    }
     return render(request, "katalog.html", context)

a8693e1cb8cfce4cbbdffe837d861196105e3336
farisumarr <muhammadfarisumarrahman@gmail.com>
2022-09-21 16:18:53 +0700

    try fix assertionError 301

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -3,13 +3,13 @@ from django.test import TestCase, Client
 # Create your tests here.
 class AppTest(TestCase):
     def test_app_html(self):
-        response = Client().get('https://tugas2pbpfaris.herokuapp.com/mywatchlist/html')
+        response = Client().get('https://tugas2pbpfaris.herokuapp.com/mywatchlist/html', follow=True)
         self.assertEqual(response.status_code,200)
 
     def test_app_xml(self):
-        response = Client().get('https://tugas2pbpfaris.herokuapp.com/mywatchlist/xml')
+        response = Client().get('https://tugas2pbpfaris.herokuapp.com/mywatchlist/xml', follow=True)
         self.assertEqual(response.status_code,200)
 
     def test_app_json(self):
-        response = Client().get('https://tugas2pbpfaris.herokuapp.com/mywatchlist/json')
+        response = Client().get('https://tugas2pbpfaris.herokuapp.com/mywatchlist/json', follow=True)
         self.assertEqual(response.status_code,200)
